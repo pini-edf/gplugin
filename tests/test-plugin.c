a9e2c12567bb270e5a57ed05d266ad532fdc930a
Gary Kramlich <grim@reaperworld.com>
2012-01-03 23:15:07 -0600

    changed the test plugin's info around so it's easy to compare

--- tests/test-plugin.c
+++ tests/test-plugin.c
@@ -1,13 +1,15 @@
 #include <gplugin.h>
 
 static const GPluginPluginInfo info = {
+	.id = "test-native-plugin",
 	.abi_version = GPLUGIN_NATIVE_PLUGIN_ABI_VERSION,
-	.name = "test plugin",
-	.summary = "a plugin to test the native loader",
-	.description = "This plugin doesn't do anything, it is just used to test "
-	               "that the native plugin loader is working correctly",
-	.author = "Gary Kramlich <grim@reaperworld.com>",
-	.website = "https://www.guifications.org/projects/gplugin/",
+	.flags = 0,
+	.name = "test plugin name",
+	.version = "test plugin version",
+	.summary = "test plugin summary",
+	.description = "test plugin description",
+	.author = "test plugin author",
+	.website = "test plugin website",
 };
 
 G_MODULE_EXPORT const GPluginPluginInfo *
