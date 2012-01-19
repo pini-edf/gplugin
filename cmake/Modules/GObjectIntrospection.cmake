044a80b9b0d9ba883278e097df9163442f90c2c6
Gary Kramlich <grim@reaperworld.com>
2012-01-18 23:15:35 -0600

    a bunch of work on the gir cmake module, doesn't work yet, but this is getting crazy... not sure what it's going to be licensed as... assume gplv3 for now, need to see what the standard license is for cmake modules since i'd rather have it there...

--- cmake/Modules/GObjectIntrospection.cmake
+++ cmake/Modules/GObjectIntrospection.cmake
@@ -1,24 +1,81 @@
+include(CMakeParseArguments)
 include(FindPkgConfig)
 
 pkg_check_modules(GOBJECT_INTROSPECTION REQUIRED gobject-introspection-1.0)
 
-find_program(G_IR_SCANNER NAMES g-ir-scanner DOC "g-ir-scanner executable")
-mark_as_advanced(G_IR_SCANNER)
-find_program(G_IR_COMPILER NAMES g-ir-compiler DOC "g-ir-compiler executable")
-mark_as_advanced(G_IR_COMPILER)
-find_program(G_IR_GENERATE NAMES g-ir-generate DOC "g-ir-generate executable")
-mark_as_advanced(G_IR_GENERATE)
+find_program(GIR_SCANNER NAMES g-ir-scanner DOC "g-ir-scanner executable")
+mark_as_advanced(GIR_SCANNER)
+find_program(GIR_COMPILER NAMES g-ir-compiler DOC "g-ir-compiler executable")
+mark_as_advanced(GIR_COMPILER)
+find_program(GIR_GENERATE NAMES g-ir-generate DOC "g-ir-generate executable")
+mark_as_advanced(GIR_GENERATE)
 
-macro(add_gir _target)
-	# grab the source directory and the sources for the target
-	get_target_property(_source_dir ${_target} SOURCE_DIR)
-	get_target_property(_sources ${_target} SOURCES)
+macro(_gir_list_prefix _newlist _list _prefix)
+	set(${_newlist})
+	foreach(_item IN LISTS ${_list})
+		list(APPEND ${_newlist} ${_prefix}${_item})
+	endforeach(_item)
+endmacro(_gir_list_prefix)
 
-	get_target_property(_target_defines ${_target} COMPILE_FLAGS)
-	get_directory_property(_directory_defines ${_target} COMPILE_FLAGS)
+function(add_gir_introspection _FIRST_ARG)
+	set(oneValueArgs FILENAME PROGRAM PROGRAM_ARG)
+	set(multiValueArgs
+		SCANNER_ARGS
+		COMPILER_ARGS
+		SOURCES
+		INCLUDES
+		CFLAGS
+		LIBS
+		FILES
+		IDENTIFIER_PREFIXES
+		SYMBOL_PREFIXES
+	)
 
-	message("target defines: " ${_target_defines})
-	message("directory defines: " ${_directory_defines})
+	CMAKE_PARSE_ARGUMENTS(GIR "" "${oneValueArgs}" "${multiValueArgs}" ${_FIRST_ARG} ${ARGN})
 
-endmacro(add_gir)
+	if(ADD_GIR_UNPARSED_ARGUMENTS)
+		message(FATAL_ERROR "Unknown keys given to ADD_GIR_INTROSPECTION(): \"${ADD_GIR_UNPARSED_ARGUMENTS}\"")
+	endif(ADD_GIR_UNPARSED_ARGUMENTS)
+
+	# if program has been set, we prepend --program on to it
+	if(GIR_PROGRAM)
+		set(GIR_PROGRAM "--program=${GIR_PROGRAM}")
+	endif(GIR_PROGRAM)
+
+	# if program_arg has been set, we prepend --program-arg on to it
+	if(GIR_PROGRAM_ARG)
+		set(GIR_PROGRAM_ARG "--program-arg=${GIR_PROGRAM_ARG}")
+	endif(GIR_PROGRAM_ARG)
+
+	###########################################################################
+	# Clean up any of the multivalue items that all need to be prefixed
+	###########################################################################
+
+	# if the user specified IDENTIFIER_PREFIXES we need to prefix each with --identifier-prefix
+	if(GIR_IDENTIFIER_PREFIXES)
+		_gir_list_prefix(GIR_REAL_IDENTIFIER_PREFIXES GIR_IDENTIFIER_PREFIXES "--identifier-prefix=")
+	endif(GIR_IDENTIFIER_PREFIXES)
+
+	# if the user specified SYMBOL_PREFIXES we need to prefix each with --symbol-prefix=
+	if(GIR_SYMBOL_PREFIXES)
+		_gir_list_prefix(GIR_REAL_SYMBOL_PREFIXES GIR_SYMBOL_PREFIXES "--symbol-prefix=")
+	endif(GIR_SYMBOL_PREFIXES)
+
+	###########################################################################
+	# Add the custom commands
+	###########################################################################
+	add_custom_command(
+		COMMAND ${GIR_SCANNER} ${GIR_SCANNER_ARGS}
+			${GIR_REAL_IDENTIFIER_PREFIXES}
+			${GIR_REAL_SYMBOL_PREFIXES}
+			--no-libtool
+			--output ${CMAKE_CURRENT_BINARY_DIR}/${GIR_FILENAME}
+		OUTPUT ${GIR_FILENAME}
+		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		VERBATIM
+	)
+
+	message("filename: ${GIR_FILENAME}")
+	message("symbol prefixes: ${GIR_REAL_SYMBOL_PREFIXES}")
+endfunction(add_gir_introspection)
 
