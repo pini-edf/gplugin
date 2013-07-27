62a3037d16ceb8831d333d51d6e6d6f2f1ac6a53
Gary Kramlich <grim@reaperworld.com>
2013-07-26 22:32:25 -0500

    fixed the pkg_config_variable macro which I thought worked but I was wrong...

--- cmake/Modules/PkgConfigVariable.cmake
+++ cmake/Modules/PkgConfigVariable.cmake
@@ -5,7 +5,7 @@ macro(pkg_config_variable MODULE VARIABLE OUTPUT_VARIABLE)
 
 	execute_process(
 		COMMAND ${PKG_CONFIG_EXECUTABLE} --variable=${VARIABLE} ${MODULE}
-		OUTPUT_VARIABLE ${_output}
+		OUTPUT_VARIABLE _output
 		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 
