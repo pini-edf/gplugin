76475fdc665ad271b4b88126aa771ee12c08457d
Gary Kramlich <grim@reaperworld.com>
2013-08-28 21:49:27 -0500

    ankit caught a typo

--- python/gplugin-python-core.c
+++ python/gplugin-python-core.c
@@ -34,7 +34,7 @@ gplugin_plugin_query(GError **error) {
 		"version", GPLUGIN_VERSION,
 		"license-id", "GPL3",
 		"summary", "A plugin that can load python plugins",
-		"description", "This plugin allows the load of plugins written in "
+		"description", "This plugin allows the loading of plugins written in "
 		               "the python programming language.",
 		"authors", authors,
 		"website", "http://bitbucket.org/rw_grim/gplugin",
