44bde0ea8b19ae3da2cdca504f2c87e89881d251
Gary Kramlich <grim@reaperworld.com>
2013-10-27 08:26:32 -0500

    some cleanups to the lua plugin

--- lua/tests/plugins/basic.lua
+++ lua/tests/plugins/basic.lua
@@ -1,3 +1,20 @@
+--[[
+ Copyright (C) 2011-2013 Gary Kramlich <grim@reaperworld.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 local lgi = require 'lgi'
 local GPlugin = lgi.GPlugin
 
@@ -7,10 +24,12 @@ function gplugin_query()
 		abi_version = 0x01020304,
 		name = "basic plugin",
 		category = "test",
-		version = "version",
+		version = "0.0.10",
 		license_id = "license-id",
 		summary = "basic lua plugin",
-		description = "description of the basic lua plugin"
+		description = "description of the basic lua plugin",
+		authors = { "Gary Kramlich <grim@reaperworld.com>" },
+		website = "http://bitbucket.org/rw_grim/gplugin/"
 	}
 
 	print(info)
